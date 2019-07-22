dda7946f9d07e33247a1556aa015b1ab116d2eac
DannyGusev <dannygu555@gmail.com>
2019-07-22 20:43:31 +0300

    added order component, fetching and rendering data from db(firebase)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -89,28 +89,12 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // //alert('You continue!');
-        // this.setState({loading: true}); 
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totaPrice,
-        //     customer: {
-        //         name: 'Danny Gusev',
-        //         phone: '0528000000',
-        //         email: 'test@test.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({loading: false, purchasing: false});
-        //     })
-        //     .catch(error => {
-        //         this.setState({loading: false, purchasing: false});
-        //     });
+        
         const queryParams = [];
         for(let i in this.state.ingredients){
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totaPrice);
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: '/checkout',
